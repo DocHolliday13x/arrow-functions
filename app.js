55391c2e1d56fa944a0a0b939624034f9c3d4013
Ryan Eastman <rking1323@gmail.com>
2023-03-08 00:51:31 +0000

    step 10 sumAndProduct function complete

--- app.js
+++ app.js
@@ -133,15 +133,20 @@ let objectLit = objFunc => ({
 // TODO: Uncomment the following line of code to see the output in the browser console
 console.log(objectLit());
 
+// let sumAndProduct = function(a, b) {
+//   let sum = a + b;
+//   let product = a * b;
+//   return [sum, product];
+// };
 
-let sumAndProduct = function(a, b) {
-  let sum = a + b;
-  let product = a * b;
-  return [sum, product];
-};
+let sumAndProduct = (a, b) => {
+  let sum = (a + b);
+  let prod = (a * b);
+  return [sum, prod];
+}
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
